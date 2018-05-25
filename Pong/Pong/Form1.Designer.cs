94da968adfa7f770e6d8ffa4c88871ca5853b18c
ahlinder <38750135+ahlinder@users.noreply.github.com>
2018-05-25 09:09:41 +0200

    Add files via upload

---
+++ Pong/Pong/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.spelplan = new System.Windows.Forms.Panel();
+            this.liv3PCB = new System.Windows.Forms.PictureBox();
+            this.liv2PCB = new System.Windows.Forms.PictureBox();
+            this.liv1PCB = new System.Windows.Forms.PictureBox();
+            this.lblMeny = new System.Windows.Forms.Label();
+            this.lblPoäng = new System.Windows.Forms.Label();
+            this.rack = new System.Windows.Forms.PictureBox();
+            this.boll = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.spelplan.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.liv3PCB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.liv2PCB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.liv1PCB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boll)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // spelplan
+            // 
+            this.spelplan.BackColor = System.Drawing.Color.Gray;
+            this.spelplan.Controls.Add(this.liv3PCB);
+            this.spelplan.Controls.Add(this.liv2PCB);
+            this.spelplan.Controls.Add(this.liv1PCB);
+            this.spelplan.Controls.Add(this.lblMeny);
+            this.spelplan.Controls.Add(this.lblPoäng);
+            this.spelplan.Controls.Add(this.rack);
+            this.spelplan.Controls.Add(this.boll);
+            this.spelplan.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.spelplan.Location = new System.Drawing.Point(0, 0);
+            this.spelplan.Name = "spelplan";
+            this.spelplan.Size = new System.Drawing.Size(1350, 729);
+            this.spelplan.TabIndex = 0;
+            // 
+            // liv3PCB
+            // 
+            this.liv3PCB.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("liv3PCB.BackgroundImage")));
+            this.liv3PCB.Location = new System.Drawing.Point(1194, 12);
+            this.liv3PCB.Name = "liv3PCB";
+            this.liv3PCB.Size = new System.Drawing.Size(40, 40);
+            this.liv3PCB.TabIndex = 6;
+            this.liv3PCB.TabStop = false;
+            // 
+            // liv2PCB
+            // 
+            this.liv2PCB.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("liv2PCB.BackgroundImage")));
+            this.liv2PCB.Location = new System.Drawing.Point(1244, 12);
+            this.liv2PCB.Name = "liv2PCB";
+            this.liv2PCB.Size = new System.Drawing.Size(40, 40);
+            this.liv2PCB.TabIndex = 5;
+            this.liv2PCB.TabStop = false;
+            // 
+            // liv1PCB
+            // 
+            this.liv1PCB.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("liv1PCB.BackgroundImage")));
+            this.liv1PCB.Location = new System.Drawing.Point(1294, 12);
+            this.liv1PCB.Name = "liv1PCB";
+            this.liv1PCB.Size = new System.Drawing.Size(40, 40);
+            this.liv1PCB.TabIndex = 4;
+            this.liv1PCB.TabStop = false;
+            // 
+            // lblMeny
+            // 
+            this.lblMeny.AutoSize = true;
+            this.lblMeny.Font = new System.Drawing.Font("Eras Medium ITC", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMeny.Location = new System.Drawing.Point(195, 99);
+            this.lblMeny.Name = "lblMeny";
+            this.lblMeny.Size = new System.Drawing.Size(0, 23);
+            this.lblMeny.TabIndex = 3;
+            // 
+            // lblPoäng
+            // 
+            this.lblPoäng.AutoSize = true;
+            this.lblPoäng.Font = new System.Drawing.Font("Eras Medium ITC", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPoäng.Location = new System.Drawing.Point(24, 25);
+            this.lblPoäng.Name = "lblPoäng";
+            this.lblPoäng.Size = new System.Drawing.Size(0, 23);
+            this.lblPoäng.TabIndex = 2;
+            // 
+            // rack
+            // 
+            this.rack.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("rack.BackgroundImage")));
+            this.rack.Location = new System.Drawing.Point(144, 276);
+            this.rack.Name = "rack";
+            this.rack.Size = new System.Drawing.Size(140, 20);
+            this.rack.TabIndex = 1;
+            this.rack.TabStop = false;
+            // 
+            // boll
+            // 
+            this.boll.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("boll.BackgroundImage")));
+            this.boll.Location = new System.Drawing.Point(170, 63);
+            this.boll.Name = "boll";
+            this.boll.Size = new System.Drawing.Size(25, 25);
+            this.boll.TabIndex = 0;
+            this.boll.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.spelplan);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.spelplan.ResumeLayout(false);
+            this.spelplan.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.liv3PCB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.liv2PCB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.liv1PCB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.boll)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel spelplan;
+        private System.Windows.Forms.PictureBox rack;
+        private System.Windows.Forms.PictureBox boll;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblPoäng;
+        private System.Windows.Forms.Label lblMeny;
+        private System.Windows.Forms.PictureBox liv1PCB;
+        private System.Windows.Forms.PictureBox liv3PCB;
+        private System.Windows.Forms.PictureBox liv2PCB;
+    }
+}
+
---
+++ Pong/Pong/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace Pong
+{
+    public partial class Form1 : Form
+    {
+        public int fart_horisontell; //Farten som är horisontell.
+        public int fart_upp; //Farten som är vertikal.
+        public int poäng = 0; //Poängen startar från 0;
+        public int liv = 3; //Starta med tre liv.
+        bool mute = false;
+        public int högpoäng = 0;
+        Random slump = new Random();
+                     
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Enabled = true; //Startar spelet
+            Cursor.Hide();
+            fart_horisontell = slump.Next(-5, 11); //Sätter farten mellan 5 och 10.
+            fart_upp = slump.Next(-5, 11); //Sätter farten mellan 5 och 10.
+            this.FormBorderStyle = FormBorderStyle.None; //Tar bort alla ramar så att spelet ser mer "clean" ut.           
+            this.Bounds = Screen.PrimaryScreen.Bounds; //Spelet är alltid i fullskärm.
+            rack.Top = spelplan.Bottom - 25; //Sätter rackets positition vid bottnen.
+
+        }        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            rack.Left = Cursor.Position.X;
+
+            boll.Left += fart_horisontell; //Gör så att "bollen" rör sig från början.
+            boll.Top += fart_upp; // -II-
+
+            if (boll.Bottom >= rack.Top && boll.Bottom <= rack.Bottom && boll.Left >= rack.Left && boll.Right <= rack.Right) //Gör så att bollen får mer fart per studs,                                                                                                                            //samt att den fastnar i botten då man förlorar (så att den inte bara fortsätter att åka i "oändlighet".
+            {
+                fart_upp += 2;
+                fart_horisontell += 2;
+                poäng += 1;
+                lblPoäng.Text = "Oliverskrik: " + poäng;
+                fart_upp = -fart_upp; //Ändrar riktningen på "bollen" så att den studsar uppåt istället för att fortsätta nedåt.
+                if (mute == false)
+                {
+                    System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"c:\Pong\oliverhoppG.wav");
+                    player.Play();
+                }
+                else
+                {
+                    System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"c:\Pong\oliverhoppG.wav");
+                    player.Stop();
+                }
+            }
+            if (boll.Left <= spelplan.Left)
+            {
+                fart_horisontell = -fart_horisontell; //Vid varje studs mot en vägg måste riktningen ändras.
+            }
+            if (boll.Right >= spelplan.Right)
+            {
+                fart_horisontell = -fart_horisontell; //Vid varje studs mot en vägg måste riktningen ändras.
+            }
+            if (boll.Top <= spelplan.Top)
+            {
+                fart_upp = -fart_upp;       //Vid varje studs mot en vägg måste riktningen ändras.      
+            }
+            if (boll.Bottom >= spelplan.Bottom) //Då bollen rör golvet är spelet över.
+            {
+                liv--;
+                boll.Left = slump.Next(10,1001);
+                boll.Top = 60;
+                fart_horisontell = slump.Next(-5, 11);
+                fart_upp = slump.Next(-5, 11);
+                timer1.Enabled = true;
+                lblMeny.Visible = false;
+                lblPoäng.Text = "Oliverskrik: " + poäng;
+                if (mute == false)
+                {
+                    System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"c:\Pong\Roblox.wav");
+                    player.Play();
+                }                             
+            }
+            if (liv == 3)
+            {
+                liv1PCB.Visible = true;
+                liv2PCB.Visible = true;
+                liv3PCB.Visible = true;
+            }
+            else if (liv == 2)
+            {
+                liv1PCB.Visible = true;
+                liv2PCB.Visible = true;
+                liv3PCB.Visible = false;
+            }
+            else if (liv == 1)
+            {
+                liv1PCB.Visible = true;
+                liv2PCB.Visible = false;
+                liv3PCB.Visible = false;
+            }
+            else if (liv == 0)
+            {
+                liv1PCB.Visible = false;
+                liv2PCB.Visible = false;
+                liv3PCB.Visible = false;
+                timer1.Enabled = false;
+
+                lblMeny.Visible = true; //Visa "menyn" då spelet är över.
+                lblMeny.Text = "ESC - Stäng av " + "\n" + " R - Starta om" + "\n" + " Z - Lycka till " + "\n" + " M - Stäng av ljud ";
+                lblPoäng.Visible = false;
+                rack.Visible = false;
+                boll.Visible = false;
+                if (mute == false)
+                {
+                    System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"c:\Pong\TadaOliver123.wav");
+                    player.Play();
+                    spelplan.BackgroundImage = Pong.Properties.Resources.oliverxd;
+                }
+                else if (mute == true)
+                {
+                    spelplan.BackgroundImage = Pong.Properties.Resources.OliverChill;
+                }
+            }
+            if (poäng % 2 == 0) //Då poängen är jämn så visas bollen som "oliverjackshitmannen.jpg" och när poängen är udda visas bollen som "oliver3.jpg".
+            {
+                boll.Image = Pong.Properties.Resources.oliverjackshitmannen;
+            }
+            else
+            {
+                boll.Image = Pong.Properties.Resources.oliver3;
+            }
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close(); //Klicka på escape så stängs spelet av.
+            }
+
+            if (e.KeyCode == Keys.R) //Sätt "bollen" vid startpositionen
+            {
+                boll.Left = slump.Next(10, 1001);
+                boll.Top = 60;
+                poäng = 0;
+                fart_horisontell = slump.Next(-5, 11); //Sätter farten mellan -5 och 10.
+                fart_upp = slump.Next(-5, 11); //Sätter farten mellan -5 och 10.
+                lblPoäng.Visible = true;
+                rack.Visible = true;
+                boll.Visible = true;
+                timer1.Enabled = true;
+                lblMeny.Visible = false;
+                lblPoäng.Text = "Oliverskrik: " + poäng;
+                mute = false;
+                liv = 3;
+                spelplan.BackgroundImage = null; //Sätter tillbaka bakgrunden till den gråa ifrån olivers ansikte.
+            }
+            if (e.KeyCode == Keys.F1) //Sätt bollen vid startpositionen men med en högre utgångsfart
+            {
+                boll.Left = slump.Next(10,1001);
+                boll.Top = 60;
+                poäng = 0;
+                fart_horisontell = slump.Next(-5, 11); //Sätter farten mellan -5 och 10.
+                fart_upp = slump.Next(-5, 11); //Sätter farten mellan -5 och 10.
+                timer1.Enabled = true;
+                lblMeny.Visible = false;
+                lblPoäng.Text = "Oliverskrik: " + poäng;
+                mute = false;
+                liv = 1;
+            }
+            if (e.KeyCode == Keys.M) //Stäng av ljudet
+            {
+                mute = true;
+            }
+            
+        }
+
+        
+    }
+}
+        
